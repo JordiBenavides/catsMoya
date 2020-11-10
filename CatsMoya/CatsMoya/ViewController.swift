30d9839763746ba88d8813b59151e8988f0cc988
Jordi Milla Catalán <jb.90@hotmail.es>
2020-11-10 18:54:55 +0100

    Install cocoa pods, Moya pod and Kingfisher pod

---
+++ CatsMoya/CatsMoya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatsMoya
+//
+//  Created by Jordi Milla on 10/11/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
